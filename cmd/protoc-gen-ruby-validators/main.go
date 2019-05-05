c6240ab39f5fdc5c8d058a01d5301c180cf70460
Yusuke Sato <yusuke1.sato9.git@gmail.com>
2019-05-05 20:52:12 +0900

    refacotring; extract core logics

--- cmd/protoc-gen-ruby-validators/main.go
+++ cmd/protoc-gen-ruby-validators/main.go
@@ -1,70 +1,24 @@
 package main
 
 import (
-    "io"
-    "io/ioutil"
     "log"
     "os"
-    "strings"
 
-    "github.com/golang/protobuf/proto"
-    descriptor "github.com/golang/protobuf/protoc-gen-go/descriptor"
-    plugin "github.com/golang/protobuf/protoc-gen-go/plugin"
+    . "github.com/sat0yu/protoc-gen-ruby-validators/plugin"
 
     // download dependencies in ${GOPATH}/src
     _ "github.com/mwitkow/go-proto-validators"
 )
 
-func parseReq(r io.Reader) (*plugin.CodeGeneratorRequest, error) {
-    buf, err := ioutil.ReadAll(r)
-    if err != nil {
-        return nil, err
-    }
-
-    var req plugin.CodeGeneratorRequest
-    if err = proto.Unmarshal(buf, &req); err != nil {
-        return nil, err
-    }
-    return &req, nil
-}
-
-func processReq(req *plugin.CodeGeneratorRequest) *plugin.CodeGeneratorResponse {
-    files := make(map[string]*descriptor.FileDescriptorProto)
-    for _, f := range req.ProtoFile {
-        files[f.GetName()] = f
-    }
-
-    var resp plugin.CodeGeneratorResponse
-    for _, fname := range req.FileToGenerate {
-        f := files[fname]
-        splitted := strings.Split(fname, ".proto")
-        out := splitted[0] + "_validators_pb.rb"
-        resp.File = append(resp.File, &plugin.CodeGeneratorResponse_File{
-            Name:    proto.String(out),
-            Content: proto.String(proto.MarshalTextString(f)),
-        })
-    }
-    return &resp
-}
-
-func emitResp(resp *plugin.CodeGeneratorResponse) error {
-    buf, err := proto.Marshal(resp)
-    if err != nil {
-        return err
-    }
-    _, err = os.Stdout.Write(buf)
-    return err
-}
-
 func run() error {
-    req, err := parseReq(os.Stdin)
+    req, err := ParseRequest(os.Stdin)
     if err != nil {
         return err
     }
 
-    resp := processReq(req)
+    resp := ProcessRequest(req)
 
-    return emitResp(resp)
+    return EmitResponse(resp)
 }
 
 func main() {
